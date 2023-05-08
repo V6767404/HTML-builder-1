35f7c756b7b2e495f317270330a387a64f363ef7
v6767404 <v6767404@gmail.com>
2023-05-08 23:16:55 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const curPath = path.join(__dirname, 'files');
+const targetPath = path.join(__dirname, 'files-copy');
 
+const copyDir = () => {
+  fs.readdir(targetPath, { withFileTypes: true }, (err, trgfiles) => {
+    if (trgfiles) {
+      trgfiles.forEach((file) => {
+        fs.unlink(path.join(targetPath, file.name), (err) => {
+          if (err) throw err;
+        });
+      });
+    }
+  });
+
+  fs.mkdir(targetPath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(curPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      fs.copyFile(path.join(curPath, file.name), path.join(targetPath, file.name), (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+};
+
+copyDir();
